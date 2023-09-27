d2358b6510df29c4feb652d61d6aa40572aadaaf
Igor Chvalun <igor.chvalun@investengine.com>
2023-09-27 11:46:49 +0530

    Done

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        if(year < 0 || month < 0 || month > 12) {
+            System.out.println("invalid date");
+            return;
+        }
+        boolean isLeap = (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
+        if(month == 6) {
+            System.out.println(30);
+        } else if(month == 2) {
+            if(isLeap) {
+                System.out.println(29);
+            } else {
+                System.out.println(28);
+            }
+        }
     }
 }
